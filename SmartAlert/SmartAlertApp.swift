ab7fa986fcaf9f818799c4d22ce5c7b5a58f7b3f
Avery Yang <averyyang75@gmail.com>
2025-01-26 21:22:15 -0500

    Initial Commit

---
+++ SmartAlert/SmartAlertApp.swift
@@ -0,0 +1,17 @@
+//
+// SmartAlertApp.swift
+// SmartAlert
+
+//Created by Avery Yang on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
