be7beded75f23b2356567666b385f1d6d3708c7f
Avery Yang <averyyang75@gmail.com>
2025-01-26 21:49:06 -0500

    First commit

--- SmartAlert/ContentView.swift
+++ SmartAlert/ContentView.swift
@@ -6,14 +6,37 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Button("Request Notification Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { _, error in
+                    if let error {
+                        print(error.localizedDescription)
+                    }
+                    
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            
+            Button("Car Honk Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Alert"
+                content.body = "Car Honk Detected"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                let id = UUID().uuidString
+                let request = UNNotificationRequest(identifier: id, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
         }
         .padding()
     }
